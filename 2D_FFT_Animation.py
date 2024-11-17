c426ff3cd5cc4302b789acd3d8d3f7ca7649ec1c
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-11-17 18:04:44 +0100

    Remove axises from plots

--- 2D_FFT_Animation.py
+++ 2D_FFT_Animation.py
@@ -31,18 +31,22 @@ def find_symmetric_coordinates(coords, centre):
 def display_plots(part_ft, acc_ft, individual_grating, reconstruction, idx):
     plt.subplot(334)
     plt.title(f"Part ft step={idx}")
+    plt.axis("off")
     plt.imshow(part_ft)
 
     plt.subplot(335)
     plt.title(f"Acc ft step={idx}")
+    plt.axis("off")
     plt.imshow(acc_ft)
 
     plt.subplot(336)
     plt.title(f"Ind step={idx}")
+    plt.axis("off")
     plt.imshow(individual_grating)
 
     plt.subplot(337)
     plt.title(f"Rec step={idx}")
+    plt.axis("off")
     plt.imshow(reconstruction)
 
     plt.pause(0.01)
@@ -79,14 +83,17 @@ ft = calculate_2dft(image)
 # Show grayscale image and its Fourier transform
 plt.subplot(331)
 plt.title("Original")
+plt.axis("off")
 plt.imshow(image)
 
 plt.subplot(332)
 plt.title("Freq domain")
+plt.axis("off")
 plt.imshow(abs(ft))
 
 plt.subplot(333)
 plt.title("log freq domain")
+plt.axis("off")
 plt.imshow(np.log(abs(ft)))
 
 # Step 1
